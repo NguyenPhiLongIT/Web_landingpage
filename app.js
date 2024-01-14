887229c67242412a7bc56f7ff9e4961f9a73da65
NguyenPhiLongIT <126060676+NguyenPhiLongIT@users.noreply.github.com>
2024-01-14 11:23:17 +0700

    update router, ejs, split file

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const ejs = require('ejs');
+const app = express();
+const port = 1009;
+
+app.set("views", path.join(__dirname, 'views'));
+app.set("view engine", 'ejs');
+app.use(express.json());
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Setup router
+app.use('/', require('./routes/home'));
+app.use('/blog', require('./routes/blog'));
+
+// app.post();
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
