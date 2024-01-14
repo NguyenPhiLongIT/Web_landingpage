1f8801009377ae81b4099964b6efaf0b8231a8ec
NguyenPhiLongIT <126060676+NguyenPhiLongIT@users.noreply.github.com>
2024-01-14 16:30:52 +0700

    add mongoose

--- app.js
+++ app.js
@@ -2,8 +2,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+
 const app = express();
 const port = 1009;
+
+// mongoose
+//     .connect('mongodb+srv://nguyenphilongit123:Long10092003@cluster0.pbxpwii.mongodb.net/LIPS')
+//     .then(() => {
+//         app.listen("Connected");
+//     })
+//     .catch((err) => {
+//         console.log("ERROR");
+//     });
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb+srv://nguyenphilongit123:Long10092003@cluster0.pbxpwii.mongodb.net/LIPS');
+}
 
 app.set("views", path.join(__dirname, 'views'));
 app.set("view engine", 'ejs');
