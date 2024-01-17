52ad5fe86da97329d53e4720f6d837be5b80e90a
Phuong <phambuinamphuong@gmail.com>
2024-01-17 21:11:56 +0700

    upgrade UI

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const ejs = require('ejs');
+// const ejs = require('ejs');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -19,14 +19,14 @@ const port = 1009;
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb+srv://nguyenphilongit123:Long10092003@cluster0.pbxpwii.mongodb.net/LIPS');
+	await mongoose.connect('mongodb+srv://nguyenphilongit123:Long10092003@cluster0.pbxpwii.mongodb.net/LIPS');
 }
 
 app.set("views", path.join(__dirname, 'views'));
 app.set("view engine", 'ejs');
 app.use(express.json());
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Setup router
@@ -36,5 +36,5 @@ app.use('/blog', require('./routes/blog'));
 // app.post();
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+	console.log(`Listening on port ${port}`);
 });
