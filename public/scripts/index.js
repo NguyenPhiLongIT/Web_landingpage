ad0245123789ec71b3eb2ed7b0697ae4e74692f9
Phuong <phambuinamphuong@gmail.com>
2024-01-20 16:45:52 +0700

    fix all home -> issue: width scroll

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -6,23 +6,23 @@ let prev = document.getElementById('prev');
 let next = document.getElementById('next');
 
 let active = 0;
-let lengthItems = items.length-1;
+let lengthItems = items.length - 1;
 
-next.onclick = function(){
-    if(active + 1 > lengthItems) 
-        active = 0; 
+next.onclick = function () {
+    if (active + 1 > lengthItems)
+        active = 0;
     else active += 1;
     reloadSlider();
 }
-prev.onclick = function(){
-    if(active - 1 < 0)
+prev.onclick = function () {
+    if (active - 1 < 0)
         active = lengthItems;
     else active -= 1;
     reloadSlider();
 }
 
-let refreshSlider = setInterval(()=> {next.click()}, 4000);
-function reloadSlider(){
+let refreshSlider = setInterval(() => { next.click() }, 10000);
+function reloadSlider() {
     let checkLeft = items[active].offsetLeft;
     slide.style.left = -checkLeft + 'px';
 
@@ -30,14 +30,14 @@ function reloadSlider(){
     lastActiveDot.classList.remove('active');
     dots[active].classList.add('active');
     clearInterval(refreshSlider);
-    refreshSlider = setInterval(()=> {next.click()}, 4000);
+    refreshSlider = setInterval(() => { next.click() }, 10000);
 }
 dots.forEach((li, key) => {
-    li.addEventListener('click', function(){
+    li.addEventListener('click', function () {
         active = key;
         reloadSlider();
     })
-}) 
+})
 
 // button prev and next in product
 let type = document.querySelector('#detail-types .type-product');
@@ -46,30 +46,30 @@ let prevProduct = document.getElementById('prev-product');
 let nextProduct = document.getElementById('next-product');
 
 let activeProduct = 0;
-let lengthProducts = contents.length-1;
+let lengthProducts = contents.length - 1;
 
-nextProduct.onclick = function(){
-    if(activeProduct + 1 > lengthProducts) 
-        activeProduct = 0; 
+nextProduct.onclick = function () {
+    if (activeProduct + 1 > lengthProducts)
+        activeProduct = 0;
     else activeProduct += 1;
     reloadSlider2();
 }
-prevProduct.onclick = function(){
-    if(activeProduct - 1 < 0)
+prevProduct.onclick = function () {
+    if (activeProduct - 1 < 0)
         activeProduct = lengthProducts;
     else activeProduct -= 1;
     reloadSlider2();
 }
 
-function reloadSlider2(){
+function reloadSlider2() {
     let checkLeft = contents[activeProduct].offsetLeft;
     type.style.left = -checkLeft + 'px';
 }
 
 // button scroll to top
 const btnScrollToTop = document.querySelector('.scroll');
-btnScrollToTop.addEventListener('click', function(){
+btnScrollToTop.addEventListener('click', function () {
     window.scrollTo({
-        top:0, left:0, behavior: 'smooth'
+        top: 0, left: 0, behavior: 'smooth'
     });
 });
